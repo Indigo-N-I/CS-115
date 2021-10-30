35f724b0ccce28dddff83045719eda960b099168
Tim <readatontim@gmail.com>
2021-10-30 14:36:41 -0700

    busy time a bit off still

--- project 2/objects.py
+++ project 2/objects.py
@@ -1,7 +1,14 @@
 import random
-import simpy
-RAND_NUM = random.Random()
-RAND_NUM.seed(1)
+import simpy, numpy
+HOG_OUT_NUM = random.Random()
+ARRIVAL_NUM = random.Random()
+UNLOAD_NUM = random.Random()
+NEXT_TRAIN_NUM = numpy.random.default_rng(256)
+HOG_OUT_NUM.seed(1)
+ARRIVAL_NUM.seed(4)
+UNLOAD_NUM.seed(16)
+# NEXT_TRAIN_NUM.seed()
+
 
 class Train(object):
     ID = 0
@@ -14,11 +21,17 @@ class Train(object):
         self.start_time = start_time
         self.id = Train.ID
         Train.ID += 1
+        print(f"train {self.id} created")
+        print(f"Current time at creation is {env.now}")
 
     def run(self):
+        next_train_time = NEXT_TRAIN_NUM.poisson(10)
+        # yield self.env.process(next_train.run())
+        if not self.start_time + next_train_time > 100000:
+            next_train = Train(self.env, self.dock, self.start_time + next_train_time)
         yield self.env.timeout(self.start_time)
         print(f"train {self.id} in system at time {self.env.now}")
-        hog_out_time = RAND_NUM.random() * 5 + 6
+        hog_out_time = HOG_OUT_NUM.random() * 5 + 6
         hog_out = self.env.timeout(hog_out_time)
         print(f"train {self.id} hogs out at time {hog_out_time}")
         with self.dock.request() as req:
@@ -29,7 +42,7 @@ class Train(object):
                 yield req | hog_out
                 if not req.triggered:
                     print(f"train {self.id} hogged out at time {self.env.now}")
-                    arrival_time = RAND_NUM.random() * 1 + 2.5
+                    arrival_time = ARRIVAL_NUM.random() * 1 + 2.5
                     hog_out = self.env.timeout(12)
                     yield self.env.timeout(arrival_time)
                     print(f"train {self.id} unhogs at {self.env.now}")
@@ -37,7 +50,7 @@ class Train(object):
 
             print(f"train {self.id} got to dock at {self.env.now}" )
             # print(f"req after satisfy {req}")
-            unload_time = RAND_NUM.random()* 1 + 4.5
+            unload_time = UNLOAD_NUM.random()* 1 + 4.5
             unload = self.env.process(self.unload(unload_time, 0))
             while not unload.triggered:
                 unload_start = self.env.now
@@ -45,7 +58,7 @@ class Train(object):
                 if not unload.triggered:
                     print(f"train {self.id} hogged out at time {self.env.now}")
                     time_passed = -(unload_start - self.env.now)
-                    arrival_time = RAND_NUM.random() * 1 + 2.5
+                    arrival_time = ARRIVAL_NUM.random() * 1 + 2.5
                     hog_out = self.env.timeout(12)
                     yield self.env.timeout(arrival_time)
                     print(f"train {self.id} unhogs at {self.env.now}")
@@ -63,18 +76,32 @@ class Train(object):
         except Exception as e:
             print("other exception happened", e)
 
-
 class Dock(simpy.Resource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.data = []
+        self.length_data = 0
+        self.busy_start= self.busy_end= self.idle_start= self.idle_end= self.idle_time= self.busy_time = 0
+        self.req = 0
+        self.released = 0
+        self.total_time = 0
+        self.prev_time = 0
 
     def request(self, *args, **kwargs):
         self.data.append((self._env.now, len(self.queue)))
-        # print("requested")
+        print("requested")
+        self.length_data -= self._env.now
+        self.req += 1
+        # assert self.prev_time < self._env.now, " Time went backwards"
+        self.total_time = self._env.now
         return super().request(*args, **kwargs)
 
     def release(self, *args, **kwargs):
+        self.total_time = self._env.now
         self.data.append((self._env.now, len(self.queue)))
-        # print("released")
+        print("released")
+        print(len(self.queue))
+        # assert self.prev_time < self._env.now, " Time went backwards"
+        self.length_data += self._env.now
+        self.released += 1
         return super().release(*args, **kwargs)
--- project 2/pysim.py
+++ project 2/pysim.py
@@ -14,23 +14,27 @@ def monitor(data, resource):
     if item[2] > data['Max Queue']:
         data['Max Queue'] = item[2]
     print(item)
-    # either there is a user currently using or user in queue
-    if (item[1] > 0 or item[2] > 0) and not (data['Prev data'][1] > 0 or data['Prev data'][2] > 0):
-        print(f"busy at time {item[0]}")
-        data['Busy Time'] -= item[0]
-    # no one in queue and not in use
-    elif item[1] == 0 and item[2] == 0:
+    # # either there is a user currently using or user in queue
+    # if (item[1] > 0 or item[2] > 0) and not (data['Prev data'][1] > 0 or data['Prev data'][2] > 0):
+    #     print(f"busy at time {item[0]}")
+    #     data['Busy Time'] -= item[0]
+    # # no one in queue and not in use
+    if item[1] == 0 and item[2] == 0:
         data['Busy Time'] += item[0]
         print(f"no longer busy at time {item[0]}, total busy time {data['Busy Time']}")
+    elif data['Prev data'][1] == data['Prev data'][2] == 0:
+        print(f"busy at time {item[0]}")
+        data['Busy Time'] -= item[0]
 
     # if the queue incrases in length
-    if item[2] > data['Prev data'][2]:
-        data["Total Length"] -= item[0]
-    elif item[2] < data['Prev data'][2]:
-        data['Total Length'] += item[0]
+    # if item[2] > data['Prev data'][2]:
+    #     data["Total Length"] -= item[0]
+    # elif item[2] < data['Prev data'][2]:
+    #     data['Total Length'] += item[0]
 
     data["Prev data"] = item
     data['total time'] = item[0]
+    data['total data'].append(item)
     
 def patch_resource(resource, pre=None, post=None):
      """Patch *resource* so that it calls the callable *pre* before each
@@ -63,18 +67,20 @@ def patch_resource(resource, pre=None, post=None):
          if hasattr(resource, name):
              setattr(resource, name, get_wrapper(getattr(resource, name)))
 
-for i in range(10):
-    a = Train(env, dock, (i+1)*2)
-    # b = Crew(env, a)
+
+
+a = Train(env, dock, 0)
 
 data = {"Max Queue":0,
         "Total Length": 0,
         "Busy Time": 0,
         "Idle Time": 0,
         "Prev data": (0,0,0),
-        "total time": 0}
+        "total time": 0,
+        "total data": [(0,0,0)]}
 monitor = partial(monitor, data)
 patch_resource(dock, post = monitor)
 
-env.run(until = 100)
+env.run(until = 100000)
 print(data)
+print(dock.length_data/dock.total_time, dock.total_time)
